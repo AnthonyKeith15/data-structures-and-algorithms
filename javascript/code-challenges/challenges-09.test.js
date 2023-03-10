44a8d16b2690527274c979e06e7e1e4f888f12db
Anthony Keith <jt1keith@gmail.com>
2023-03-09 21:17:49 -0800

    challenge 09

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -67,7 +67,11 @@ HR has asked you to change the data to make it easier to print so that it looks 
 ------------------------------------------------------------------------------------------------ */
 
 const updateNumbers = (obj) => {
-  // Solution code here...
+  let phoneBookArr = Object.keys(obj).map((name) => {
+    let number = obj[name];
+    return `${name}: ${number}`;
+  });
+  return phoneBookArr;
 };
 
 
